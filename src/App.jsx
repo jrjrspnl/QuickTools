f08963af90e32b79df5f60a0a1a55f02695b7a33
jrjrspnl <jerwinespenile@gmail.com>
2025-08-05 00:57:39 +0800

    Add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+      <div>
+        <NavBar />
+        <Hero />
+      </div>
+    </div>
+  );
 };
 
 export default App;
