c58206a3448f34a92263422edfc96f3b766f4521
jrjrspnl <jerwinespenile@gmail.com>
2025-08-05 15:46:46 +0800

    Add card contents contants

---
+++ src/constants/CardContents.jsx
@@ -0,0 +1,26 @@
+import { TbBackground } from "react-icons/tb";
+import { SiConvertio } from "react-icons/si";
+import { CgCompressRight } from "react-icons/cg";
+export const CardsContent = [
+  {
+    id: 1,
+    icon: <TbBackground size={80} />,
+    title: "Remove Background",
+    description:
+      "Easily remove the background from any image in seconds with just one click.",
+  },
+  {
+    id: 2,
+    icon: <SiConvertio size={80} />,
+    title: "Convert Files",
+    description:
+      "Convert image formats such as PNG, JPG, WebP, and more in one click.",
+  },
+  {
+    id: 3,
+    icon: <CgCompressRight size={80} />,
+    title: "Image Compressor",
+    description:
+      "Shrink your image file size by reducing quality — perfect for faster uploads and saving storage.",
+  },
+];
