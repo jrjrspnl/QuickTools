54002c4f91c29fe76d221cc5a44876d784c05c7c
jrjrspnl <jerwinespenile@gmail.com>
2025-08-05 15:56:42 +0800

    Add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="flex justify-center py-4">
+      <h1 className="text-xs text-neutral-400">
+        © 2025 • Built with ❤️ by Jerwin Espenile
+      </h1>
+    </div>
+  );
+};
+
+export default Footer;
