036cc677a66d69fa9da529f6b562c08193aae061
jrjrspnl <jerwinespenile@gmail.com>
2025-08-06 14:24:19 +0800

    Changed text title color

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,22 @@
 import React from "react";
-
 const Hero = () => {
   return (
-    <div className="flex justify-center items-center px-5">
-      <div className="flex flex-col items-center max-w-4xl">
-        <h1 className="mt-10 text-4xl md:text-5xl text-center font-medium">
-          No ads, no bloat,{" "}
-          <span className="font-semibold bg-gradient-to-r from-violet-400 to-blue-400 bg-clip-text text-transparent">
-            just image editing that works.
-          </span>
-        </h1>
-        <p className="text-sm md:text-md text-center max-w-2xl my-5 text-neutral-600">
-          Fast, simple, and no BS. Remove backgrounds, convert between formats,
-          and compress your images — all without the annoying ads.
-        </p>
+    <>
+      <div className="flex justify-center items-center px-5">
+        <div className="flex flex-col items-center max-w-4xl">
+          <h1 className="mt-10 text-4xl md:text-5xl text-center font-medium text-neutral-800">
+            No ads, no bloat,{" "}
+            <span className="font-semibold bg-gradient-to-r from-violet-400 to-blue-400 bg-clip-text text-transparent">
+              just image editing that works.
+            </span>
+          </h1>
+          <p className="text-sm md:text-md text-center max-w-2xl my-5 text-neutral-600">
+            Fast, simple, and no BS. Remove backgrounds, convert between
+            formats, and compress your images — all without the annoying ads.
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
