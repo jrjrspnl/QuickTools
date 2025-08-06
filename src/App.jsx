ba69432e297d459a2bc5c872ea87909ae3c94f77
jrjrspnl <jerwinespenile@gmail.com>
2025-08-06 15:07:09 +0800

    Fixed bg gridlines to mobile

--- src/App.jsx
+++ src/App.jsx
@@ -11,27 +11,28 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
-        <div className="min-h-screen flex flex-col">
-          <NavBar />
-          <div className="flex-1 flex flex-col max-w-7xl mx-auto gap-10 w-full">
-            <Routes>
-              <Route
-                path="/"
-                element={
-                  <>
-                    <Hero />
-                    <Cards />
-                  </>
-                }
-              />
-              <Route path="/bg-remover" element={<BgRemover />} />
-              <Route path="/file-converter" element={<FileConverter />} />
-              <Route path="/file-compressor" element={<FileCompressor />} />
-            </Routes>
-          </div>
-          <Footer />
+      {/* Fixed background that covers entire viewport */}
+      <div className="fixed inset-0 -z-10 bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]" />
+
+      <div className="min-h-screen flex flex-col">
+        <NavBar />
+        <div className="flex-1 flex flex-col max-w-7xl mx-auto gap-10 w-full px-4 sm:px-6 lg:px-8">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Hero />
+                  <Cards />
+                </>
+              }
+            />
+            <Route path="/bg-remover" element={<BgRemover />} />
+            <Route path="/file-converter" element={<FileConverter />} />
+            <Route path="/file-compressor" element={<FileCompressor />} />
+          </Routes>
         </div>
+        <Footer />
       </div>
     </BrowserRouter>
   );
