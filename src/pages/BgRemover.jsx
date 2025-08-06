1aebc52d8759570cfe124a0812fe7bdcc354cde4
jrjrspnl <jerwinespenile@gmail.com>
2025-08-06 14:23:53 +0800

    Add BGRemover page component

---
+++ src/pages/BgRemover.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const BgRemover = () => {
+  return (
+    <div className="flex justify-center mt-10">
+      <h1 className="text-4xl max-w-2xl font-semibold text-center text-neutral-800">
+        Upload an image to remove the background
+      </h1>
+    </div>
+  );
+};
+
+export default BgRemover;
