363b539cd35a386c6c7ddbdfede96730f4f03700
jrjrspnl <jerwinespenile@gmail.com>
2025-08-06 15:03:43 +0800

    Fixed footer padding

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="flex justify-center py-10">
+    <div className="flex justify-center py-10 lg:py-5">
       <h1 className="text-xs text-neutral-400">
         © 2025 • Built with ❤️ by Jerwin Espenile
       </h1>
