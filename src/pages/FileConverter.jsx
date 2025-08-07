07782fec409047c5044dd6dc5f71251aa0cd3ac6
jrjrspnl <jerwinespenile@gmail.com>
2025-08-07 15:35:15 +0800

    Use the reuseable cards

--- src/pages/FileConverter.jsx
+++ src/pages/FileConverter.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-
+import { UploadImage } from "../components/Upload";
+import ConvertFiles from "../assets/convert-files.png";
 const FileConverter = () => {
-  return <div>FileConvserter</div>;
+  return (
+    <UploadImage
+      heading="Select an image to convert to a different format"
+      image={ConvertFiles}
+    />
+  );
 };
 
 export default FileConverter;
