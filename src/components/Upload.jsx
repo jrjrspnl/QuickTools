0ba98898f0bed2775ce2072d8f6c898905328873
jrjrspnl <jerwinespenile@gmail.com>
2025-08-08 23:06:15 +0800

    Add a drag and drop

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,25 +1,72 @@
+import React, { useCallback, useState } from "react";
 import { LuImagePlus } from "react-icons/lu";
+import { useDropzone } from "react-dropzone";
+
 export const UploadImage = ({ heading, image }) => {
+  const [files, setFiles] = useState([]);
+
+  const onDrop = useCallback((acceptedFiles) => {
+    console.log(acceptedFiles);
+    setFiles(acceptedFiles);
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
   return (
-    <div className="flex justify-center lg:justify-around mt-10 lg:my-20 gap-10 px-5 text-neutral-800 items-center ">
+    <div
+      className={
+        files.length === 0
+          ? "flex justify-center lg:justify-around mt-10 lg:my-20 gap-10 px-5 text-neutral-800 items-center "
+          : "flex justify-center lg:justify-around mt-10 lg:my-10 gap-10 px-5 text-neutral-800 items-center "
+      }
+    >
       <img
         src={image}
-        className="w-96 rounded-4xl -rotate-5 translate-y-10 hidden lg:flex shadow-lg transition hover:scale-105 duration-300"
+        className={
+          files.length === 0
+            ? "w-96 rounded-4xl -rotate-5 translate-y-10 hidden lg:flex shadow-lg transition hover:scale-105 duration-300"
+            : "hidden"
+        }
         alt=""
       />
+
       <div className="flex flex-col gap-5">
         <h1 className="text-3xl md:text-4xl max-w-2xl font-semibold text-center">
           {heading}
         </h1>
-        <div className="h-64 w-full  bg-neutral-50 rounded-xl shadow-xl shadow-blue-300/60 border-2 border-neutral-300 border-dashed">
-          <div className="flex flex-col items-center text-center justify-center h-full gap-5">
-            <button className="text-white cursor-pointer bg-violet-400 font-base py-2 px-5 flex items-center gap-2 rounded-full shadow-md shadow-violet-500 hover:bg-violet-500 transition-colors duration-300">
-              <LuImagePlus size={20} />
-              Upload Image
-            </button>
-            <p className="text-sm font-base">Or drop an image here</p>
+
+        {files.length === 0 ? (
+          <div
+            {...getRootProps()}
+            className={`h-64 w-full  bg-neutral-50 rounded-xl shadow-xl border-2  border-dashed ${
+              isDragActive
+                ? "border-violet-400 shadow-violet-300"
+                : "border-neutral-300 shadow-blue-300/60"
+            }`}
+          >
+            <div className="flex flex-col items-center text-center justify-center h-full gap-5">
+              <input {...getInputProps()} />
+              <button className="text-white cursor-pointer bg-violet-400 font-base py-2 px-5 flex items-center gap-2 rounded-full shadow-md shadow-violet-500 hover:bg-violet-500 transition-colors duration-300">
+                <LuImagePlus size={20} />
+                Upload Image
+              </button>
+              {isDragActive ? (
+                <p className="text-sm font-base">Drop the files here ...</p>
+              ) : (
+                <p className="text-sm font-base">Or drop an image here</p>
+              )}
+            </div>
           </div>
-        </div>
+        ) : (
+          <div className="flex items-center justify-center py-5">
+            {files.map((file) => (
+              <img
+                src={URL.createObjectURL(file)}
+                alt={file.name}
+                className="w-full object-cover rounded-lg border-1 border-violet-400"
+              />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
