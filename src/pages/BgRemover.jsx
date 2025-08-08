b57ce373e84bfc86d07d8f336628e6ebf162f6a7
jrjrspnl <jerwinespenile@gmail.com>
2025-08-08 23:06:25 +0800

    Add a loading

--- src/pages/BgRemover.jsx
+++ src/pages/BgRemover.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import alden from "../assets/bg-remove-alden.png";
 import { UploadImage } from "../components/Upload";
+
 const BgRemover = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+
   return (
-    <UploadImage
-      heading="Upload an image to remove the background"
-      image={alden}
-    />
+    <div>
+      {loading ? (
+        <h1>Loading....</h1>
+      ) : (
+        <UploadImage
+          heading="Upload an image to remove the background"
+          image={alden}
+        />
+      )}
+    </div>
   );
 };
 
