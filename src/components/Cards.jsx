bb5d91bb17c2ca957ff9e3a0cc0c8f996afeec06
jrjrspnl <jerwinespenile@gmail.com>
2025-08-09 22:26:55 +0800

    Adjust cards gap

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Cards = () => {
   return (
     <div className="flex justify-center">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 cursor-pointer gap-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 cursor-pointer gap-20">
         {CardsContent.map((content) => (
           <Link to={content.route} key={content.id}>
             <div
