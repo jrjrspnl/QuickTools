019bc629c174aef67ece8c95d9b5bbde7768fec2
jrjrspnl <jerwinespenile@gmail.com>
2025-08-13 21:22:38 +0800

    Add a buttons

--- src/pages/FileConverter.jsx
+++ src/pages/FileConverter.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { UploadImage } from "../components/Upload";
 import ConvertFiles from "../assets/convert-files.png";
 import { ImGift } from "react-icons/im";
-import { CircleX } from "lucide-react";
+import { CircleX, Download, FilePlus } from "lucide-react";
 const FileConverter = () => {
   const [selectedFile, setSelectedFile] = useState([]); // for preview
 
@@ -27,7 +27,14 @@ const FileConverter = () => {
           onDropFiles={handleFileDrop}
         />
       ) : (
-        <div className="mt-10">
+        <div className="mt-10 max-w-xl mx-auto w-full">
+          <div className="flex justify-between items-center">
+            <button className="cursor-pointer border-2 border-violet-400 py-1 px-5 rounded-lg hover:bg-violet-500 transition-colors duration-300 text-neutral-600 hover:text-white flex items-center gap-2">
+              <FilePlus size={16} />
+              Add more files
+            </button>
+          </div>
+
           {selectedFile.map((upload) => (
             <div className="text-sm md:text-base flex justify-between border border-violet-400 rounded-md  bg-white max-w-2xl mx-auto  text-neutral-600 p-3 my-2">
               <div className="flex flex-col max-w-5xl ">
@@ -46,6 +53,15 @@ const FileConverter = () => {
               </div>
             </div>
           ))}
+          <div className="flex justify-between items-center mt-3">
+            <h1 className="text-sm text-neutral-500">
+              Added {selectedFile.length} Files
+            </h1>
+            <button className="text-white cursor-pointer bg-violet-400 py-2 px-6 rounded-full hover:bg-violet-500 transition-colors duration-300 flex items-center gap-2">
+              <Download size={16} />
+              Convert
+            </button>
+          </div>
         </div>
       )}
     </>
